9db09780494fd2113df9162c3b411d150494b926
Justin Jameson <jjameson@uw.edu>
2019-01-15 15:33:32 -0800

    Justin puzzel when we sleep in

---
+++ JustinJameson-A01/Sleep_In.py
@@ -0,0 +1,10 @@
+def sleep_in(weekday, vacation):
+  
+# sleep in if week day is false or or vacation is true
+  
+    if not weekday or vacation:
+    
+        return True
+  
+    else: return False
+  
