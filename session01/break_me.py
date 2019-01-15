8cfcb132256160f672b28a9cbca13368f4e60819
johnwachter <johnwachter29@gmail.com>
2019-01-14 20:33:23 -0800

    First Homework Assignment

---
+++ session01/break_me.py
@@ -0,0 +1,22 @@
+#Title: break_me.py
+#Change Log: (Who, When, What)
+#JWachter, 2019-01-14, Created File and finished exercise
+
+""""#NameEror
+def nameerror():
+    return x
+nameerror()
+#TypeError
+def typeerror():
+    a = 'hi'
+    return a + 7
+typeerror()
+#SyntaxError
+def syntaxerror():
+    del = 'this should cause an error'
+syntaxerror()"""
+#AttributeError
+def attributeerror():
+    num = 5
+    num.lowercase
+attributeerror()
