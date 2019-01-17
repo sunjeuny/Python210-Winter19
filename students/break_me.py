7419fc6bef2ffefbb38dbcb754b79315c109468f
Kosin Chen <kokoshin07@gmail.com>
2019-01-16 16:48:45 -0800

    submitted first assignment

---
+++ students/break_me.py
@@ -0,0 +1,25 @@
+#_______________________________________#
+# Title: Task 1: Explore Errors
+# Change Log:
+# Kosin Chen, 2019-01-15, Task 1 Explore Errors
+#_______________________________________#
+
+# SyntaxError #
+
+while True print('I love Python')
+
+
+# Name Error #
+
+Name = inpt ("Enter your name")
+print ("your name is"  + Name)
+
+
+# Type Error #
+
+a=float(["14","45",34,true])
+
+
+# AttributeError#
+a = 10
+a.append(10)
